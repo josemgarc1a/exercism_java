a7673e47b0fc6a11f2299546a9f4411685824c2a
Jose Garcia <josemgarc1a@icloud.com>
2025-11-13 08:11:12 -0600

    Jedlinks Toy Car Polish

--- jedliks-toy-car/src/main/java/JedliksToyCar.java
+++ jedliks-toy-car/src/main/java/JedliksToyCar.java
@@ -1,16 +1,13 @@
 public class JedliksToyCar {
 
+    private static final int EMPTY_BATTERY = 0;
     private static final int INITIAL_DISTANCE_DRIVEN = 0;
     private static final int INITIAL_BATTERY_CHARGE = 100;
     private static final int BATTERY_USAGE_PER_DRIVE = 1;
     private static final int METERS_PER_DRIVE = 20;
-    private int batteryCharge;
-    private int metersDriven;
     
-    public JedliksToyCar() {
-        batteryCharge = INITIAL_BATTERY_CHARGE;
-        metersDriven = INITIAL_DISTANCE_DRIVEN;
-    }
+    private int batteryCharge = INITIAL_BATTERY_CHARGE;
+    private int metersDriven = INITIAL_DISTANCE_DRIVEN;
 
     public static JedliksToyCar buy() {
         return new JedliksToyCar();
@@ -21,7 +18,7 @@ public class JedliksToyCar {
     }
 
     public String batteryDisplay() {
-        if(batteryCharge==0) {
+        if(batteryCharge==EMPTY_BATTERY) {
             return "Battery empty";
         } else {
             return "Battery at %d%%".formatted(batteryCharge);
@@ -29,9 +26,9 @@ public class JedliksToyCar {
     }
 
     public void drive() {
-        if(batteryCharge>0) {
-            metersDriven += METERS_PER_DRIVE;
-            batteryCharge -= BATTERY_USAGE_PER_DRIVE;
-        }
+        if(batteryCharge==EMPTY_BATTERY) return;
+
+        metersDriven += METERS_PER_DRIVE;
+        batteryCharge -= BATTERY_USAGE_PER_DRIVE;
     }
 }
