e81143982b002066c69c1b14ee15640033406275
Jose Garcia <josemgarc1a@icloud.com>
2025-11-17 07:52:12 -0600

    Cars Assemble Solution

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,27 @@
 public class CarsAssemble {
 
+    private static final int CARS_PER_HOUR = 221;
+
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        return speed * CARS_PER_HOUR * getSuccessRate(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) Math.floor(productionRatePerHour(speed) / 60);
+    }
+
+    private static double getSuccessRate(int speed) {
+        switch (speed) {
+            case 1, 2, 3, 4:
+                return 1.0;
+            case 5, 6, 7, 8:
+                return 0.9;
+            case 9:
+                return 0.8;
+            case 10:
+                return 0.77;
+            default:
+                return 0;
+        }
     }
 }
