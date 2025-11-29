229b5e6426184bacdbfee63e34edf30e6d7ab310
Jose Garcia <josemgarc1a@icloud.com>
2025-11-29 09:41:34 -0600

    Tim From Marketing Polish

--- tim-from-marketing/src/main/java/Badge.java
+++ tim-from-marketing/src/main/java/Badge.java
@@ -1,5 +1,17 @@
+import java.util.Objects;
+
 class Badge {
+
+    public static final String SPACER = " - ";
+
     public String print(Integer id, String name, String department) {
-        throw new UnsupportedOperationException("Please implement the Badge.print() method");
+        String idText = Objects.isNull(id) ? "" : getId(id);
+        String nameText = name + SPACER;
+        String departmentText = Objects.isNull(department) ? "OWNER" : department.toUpperCase();
+        return idText+nameText+departmentText;
+    }
+
+    private String getId(Integer id) {
+        return "["+id.toString()+"]"+SPACER;
     }
 }
