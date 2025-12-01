2047d3bffcf7c0ee0d0cedebef73035d64c01cec
Jose Garcia <josemgarc1a@icloud.com>
2025-12-01 07:33:52 -0600

    Captains Log Polished

--- captains-log/src/main/java/CaptainsLog.java
+++ captains-log/src/main/java/CaptainsLog.java
@@ -3,6 +3,11 @@ import java.util.Random;
 class CaptainsLog {
 
     private static final char[] PLANET_CLASSES = new char[]{'D', 'H', 'J', 'K', 'L', 'M', 'N', 'R', 'T', 'Y'};
+    private static final String REGISTRY_PREFIX = "NCC-";
+    private static final int REGISTRY_LOW_BOUND = 1000;
+    private static final int REGISTRY_HIGH_BOUND = 10000;
+    private static final double DATE_LOW_BOUND = 41000.0;
+    private static final double DATE_HIGH_BOUND = 42000.0;
 
     private Random random;
 
@@ -11,14 +16,14 @@ class CaptainsLog {
     }
 
     char randomPlanetClass() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomPlanetClass() method");
+        return PLANET_CLASSES[random.nextInt(PLANET_CLASSES.length)];
     }
 
     String randomShipRegistryNumber() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomShipRegistryNumber() method");
+        return REGISTRY_PREFIX + random.nextInt(REGISTRY_LOW_BOUND,REGISTRY_HIGH_BOUND);
     }
 
     double randomStardate() {
-        throw new UnsupportedOperationException("Please implement the CaptainsLog.randomStardate() method");
+        return random.nextDouble(DATE_LOW_BOUND, DATE_HIGH_BOUND);
     }
 }
